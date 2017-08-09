9df1bcab9536c310d45f6aec5d1af51e0bfeda14
sliceart_polishchuk <dpolishchuk@sliceart.com.ua>
2017-08-09 16:46:43 +0300

    The first version of the component NavSearch

--- src/components/containers/TopHeader/TopHeader.js
+++ src/components/containers/TopHeader/TopHeader.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import MainLogo from '../MainLogo/MainLogo';
+import NavSearch from '../NavSearch/NavSearch';
 import PTButtonIcon from '../PTButtonIcon/PTButtonIcon';
 
 const TopHeader = props => <div className="navbar">
   <MainLogo />
+  <NavSearch />
   <PTButtonIcon classButtonName="btn-notification" icon="fa-bell-o" />
   <PTButtonIcon classButtonName="btn-user" icon="fa-user" />
 </div>;
