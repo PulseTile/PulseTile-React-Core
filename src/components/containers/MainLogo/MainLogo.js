debfd2d1cca4e108b9e3e004258cba94df3dfdb3
sliceart_polishchuk <dpolishchuk@sliceart.com.ua>
2017-08-09 15:49:11 +0300

    create MainLogo component

---
+++ src/components/containers/MainLogo/MainLogo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainLogo = props => <div className="wrap-logo">
+  <div className="logo">
+    <div className="logo-icon">
+      <a href="#">
+        <img src="./src/assets/images/ripple-foundation-logo.png" className="img" alt="logo"/>
+      </a>
+    </div>
+  </div>
+</div>;
+
+export default MainLogo;
--- src/components/containers/TopHeader/TopHeader.js
+++ src/components/containers/TopHeader/TopHeader.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import MainLogo from '../MainLogo/MainLogo';
 
 const TopHeader = props => <div className="navbar">
-
+  <MainLogo />
 </div>;
 
 export default TopHeader;
