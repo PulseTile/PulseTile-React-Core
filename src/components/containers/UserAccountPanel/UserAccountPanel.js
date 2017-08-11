2b78d5a13cee6857fde9e544d59e91dc6dcf962b
sliceart_polishchuk <dpolishchuk@sliceart.com.ua>
2017-08-11 11:40:02 +0300

    create userPanelItem component

--- src/components/containers/UserAccountPanel/UserAccountPanel.js
+++ src/components/containers/UserAccountPanel/UserAccountPanel.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import PTButton from '../PTButton/PTButton';
+import UserPanelItem from '../UserPanelItem/UserPanelItem';
 
 const UserAccountPanel = props => <ul className="user-panel" role="tablist">
-  <li className="user-panel-item visible-xs">
-    <PTButton className="btn-header"
-      children={(
-        <i className="fa fa-search"/>
-      )}
-    />
-  </li>
-  <li className="user-panel-item">
-    <PTButton className="btn-header btn-notification"
-      children={(
-        <div>
-          <i className="fa fa-bell-o"/>
-          <span className="count">2</span>
-        </div>
-      )}
-    />
-  </li>
-  <li className="user-panel-item">
-    <PTButton className="btn-header btn-user"
-      children={(
-        <i className="fa fa-user"/>
-      )}
-    />
-  </li>
+  <UserPanelItem className="user-panel-item visible-xs"
+    children={(
+      <PTButton className="btn-header"
+        children={(
+          <i className="fa fa-search"/>
+        )}
+      />
+    )}
+  />
+  <UserPanelItem className="user-panel-item"
+    children={(
+      <PTButton className="btn-header btn-notification"
+        children={(
+          <div>
+            <i className="fa fa-bell-o"/>
+            <span className="count">2</span>
+          </div>
+        )}
+      />
+    )}
+  />
+  <UserPanelItem className="user-panel-item"
+    children={(
+      <PTButton className="btn-header btn-user"
+        children={(
+          <i className="fa fa-user"/>
+        )}
+      />
+    )}
+  />
 </ul>;
 
 export default UserAccountPanel;
