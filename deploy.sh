ed2efd253d56b5a7cbe30fc32cf068b8908a6fae
polesskiy-dev <polesskiy.dev@gmail.com>
2017-08-18 21:20:15 +0300

    add deploy to dev droplet and start creating a webpack prod script

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+rsync -r ./dist/ root@46.101.4.145:app/public/
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const sourcePath = path.join(__dirname, 'src');
 const buildPath = path.join(__dirname, 'dist');
@@ -9,29 +10,53 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: path.resolve(sourcePath, 'index.html'),
   filename: path.resolve(buildPath, 'index.html'),
   inject: 'body',
-})
+});
+
+const DEV_SERVER_URL = 'http://46.101.95.245';
 
 module.exports = {
   devtool: 'source-map',
 
-  entry: {
-    app: path.resolve(sourcePath, 'index.js'),
-  },
+  entry: [
+    path.resolve(sourcePath, 'index.js'),
+  ],
 
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.resolve(buildPath),
     filename: '[name].js',
     publicPath: '/',
   },
 
-  plugins: [HtmlWebpackPluginConfig],
+  plugins: [
+    HtmlWebpackPluginConfig,
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+
+    new webpack.NoEmitOnErrorsPlugin(),
+    // do not emit compiled assets that include errors
+
+    new ExtractTextPlugin('styles.css'),
+  ],
 
   module: {
     loaders: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loaders: ['react-hot-loader/webpack', 'babel-loader'],
+      },
+      {
+        test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+        loader: 'url-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          //resolve-url-loader may be chained before sass-loader if necessary
+          use: ['css-loader?sourceMap', 'sass-loader?sourceMap'],
+        }),
       },
     ],
   },
