b0641787bc3f4b67a0acb5e927e956eb8ae1eb2b
sliceart_polishchuk <dpolishchuk@sliceart.com.ua>
2017-09-07 15:28:24 +0300

    create user account panel

---
+++ src/components/containers/UserPanel/UserAccountPanel.js
@@ -0,0 +1,43 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { lifecycle } from 'recompose';
+
+import PTButton from '../../ui-elements/PTButton/PTButton';
+import userAccountSelector from './selectors';
+import { unmountOnBlur } from '../../../utils/HOCs/unmount-on-blur.utils';
+
+@connect(userAccountSelector)
+@lifecycle(unmountOnBlur)
+export default class UserAccountPanel extends PureComponent {
+  static propTypes = {
+    user: PropTypes.shape().isRequired,
+  };
+  render() {
+    const { user } = this.props;
+    return (
+      <div className="dropdown-user dropdown-menu-right dropdown-menu">
+        <div className="user-profile-image">
+          <div className="img">
+            <img src={require('../../../assets/images/user.jpg')} alt="" />
+          </div>
+        </div>
+        <div className="user-profile-info">
+          <div className="name">{user.given_name} {user.family_name}</div>
+          <div className="specification">
+            <div className="item"><em>{user.role}</em></div>
+            <div className="item">{user.email}</div>
+            <div className="item gray"><em>10/05/2099</em></div>
+            <div className="item"><em>About Showcase Stack; PulseTile version 1.0.0/QEWD_Ripple version 1.0.0</em></div>
+          </div>
+          <PTButton className="btn btn-success btn-block btn-signout">
+            <div>
+              <span className="brn-text">Sign Out</span>
+              <i className="btn-icon fa fa-sign-out" />
+            </div>
+          </PTButton>
+        </div>
+      </div>
+    )
+  }
+}
