0bc109bb6ea21ad6a296e3ed4e8915e116320cee
sliceart_polishchuk <dpolishchuk@sliceart.com.ua>
2017-09-29 13:51:28 +0300

    1. Create <AllergiesListHeader /> 2. Added config for allergies table 3.Added sidebar to <Allergies />

--- src/components/pages/Allergies/Allergies.js
+++ src/components/pages/Allergies/Allergies.js
@@ -1,7 +1,37 @@
 import React, { PureComponent } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import _ from 'lodash/fp';
+
+import AllergiesListHeader from './header/AllergiesListHeader';
+import SortableTable from '../../containers/SortableTable/SortableTable';
+import { allergiesColumnsConfig, defaultColumnsSelected } from '../../../config/allergies-table-columns.config'
 
 export default class Allergies extends PureComponent {
+  state = {
+    nameShouldInclude: '',
+    selectedColumns: defaultColumnsSelected,
+  };
+  handleFilterChange = ({ target: { value } }) => this.setState({ nameShouldInclude: _.toLower(value) });
+
   render() {
-    return (<span>HELLO Allergies</span>)
+    const { selectedColumns } = this.state;
+    const columnsToShowConfig = allergiesColumnsConfig.filter(columnConfig => selectedColumns[columnConfig.key]);
+    return (<section className="page-wrapper">
+      <Row>
+        <Col xs={12}>
+          <div className="panel panel-primary">
+            <AllergiesListHeader
+              onFilterChange={this.handleFilterChange}
+              panelTitle="Allergies"
+            />
+            <div className="panel-body">
+              <SortableTable
+                headers={columnsToShowConfig}
+              />
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </section>)
   }
 }
