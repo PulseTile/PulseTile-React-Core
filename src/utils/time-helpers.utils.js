5e84859ce8717a18f5fe670bee709fd5b6e4117a
polesskiy-dev <polesskiy.dev@gmail.com>
2017-09-29 14:16:28 +0300

    <AdvancedSearchPage> add  search by names

--- src/utils/time-helpers.utils.js
+++ src/utils/time-helpers.utils.js
@@ -1,5 +1,7 @@
 import moment from 'moment';
 
+export const getUTCDate = dateOfBirth => moment(dateOfBirth).utc()
+
 export const getAgeYears = dateOfBirth => moment().diff(dateOfBirth, 'years');
 
 export const getDDMMMYYYY = dateOfBirth => (moment(dateOfBirth).isValid()
