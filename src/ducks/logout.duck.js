d45810287d5b5ffc26afbcf2e71c2e675bd55aac
polesskiy-dev <e34dream@gmail.com>
2017-10-12 18:33:12 +0300

    Update logout.duck.js
    fix cookie delete

--- src/ducks/logout.duck.js
+++ src/ducks/logout.duck.js
@@ -8,7 +8,7 @@ export const logoutStart = createAction(LOGOUT_START);
 export const logoutFinish = createAction(LOGOUT_FINISH);
 
 //TODO this is cause /react-ui isn't root path and session cookie could be deleted only from /
-const clearCookie = () => 'JSESSIONID=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
+const clearCookie = () => document.cookie = 'JSESSIONID=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
 
 export const logoutEpic = (action$, store) =>
   action$.ofType(LOGOUT_START)
