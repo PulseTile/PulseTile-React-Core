b52fa7ddda0faeb47ef8f707152974fede18b942
sliceart_polishchuk <dpolishchuk@sliceart.com.ua>
2017-10-16 12:34:37 +0300

    without table issues

--- src/components/containers/NavSearch/NavSearch.js
+++ src/components/containers/NavSearch/NavSearch.js
@@ -5,6 +5,7 @@ import SearchOptions from './SearchOptions'
 import PTButton from '../../ui-elements/PTButton/PTButton';
 import BasicPatientSearch from '../BasicPatientSearch/BasicPatientSearch';
 import AdvancedPatientSearch from '../AdvancedPatientSearch/AdvancedPatientSearch';
+import { isIDCRRole } from '../../../utils/auth/auth-check-permissions';
 
 const BASIC_SEARCH = 'basicSearch';
 const ADVANCED_SEARCH = 'advancedSearch';
@@ -20,17 +21,25 @@ export default class NavSearch extends PureComponent {
     document.body.click();
   }
 
+
   render() {
     const { selected } = this.state;
+    const { userAccount } = this.props;
 
     return <div className="wrap-search wrap-header-search">
       <div className="header-search">
         <div className="control-group left control-search-select dropdown">
-          <Dropdown content={<SearchOptions onSelect={this.handleSelect} {...{ BASIC_SEARCH, ADVANCED_SEARCH }} />}>
+          {isIDCRRole(userAccount) ?
+            <Dropdown content={<SearchOptions onSelect={this.handleSelect} {...{ BASIC_SEARCH, ADVANCED_SEARCH }} />}>
+              <PTButton className="btn btn-dropdown-toggle btn-search-toggle">
+                <i className="btn-icon fa fa-bars" />
+              </PTButton>
+            </Dropdown>
+            :
             <PTButton className="btn btn-dropdown-toggle btn-search-toggle">
               <i className="btn-icon fa fa-bars" />
             </PTButton>
-          </Dropdown>
+          }
         </div>
         { selected === BASIC_SEARCH && <BasicPatientSearch /> }
         { selected === ADVANCED_SEARCH && <AdvancedPatientSearch onClose={() => this.handleSelect(BASIC_SEARCH)} /> }
