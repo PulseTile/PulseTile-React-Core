918da01e7d7465fb9a76904e307d2f865019eb47
AlexGoncharov <aloshka2012@yandex.ru>
2017-11-13 10:41:33 +0200

    Change ducks of Allergies.

--- src/components/pages/Allergies/ducks/fetch-patient-allergies.duck.js
+++ src/components/pages/Allergies/ducks/fetch-patient-allergies.duck.js
@@ -4,7 +4,7 @@ import { ajax } from 'rxjs/observable/dom/ajax';
 import { createAction } from 'redux-actions';
 
 import { usersUrls } from '../../../../config/server-urls.constants';
-import { FETCH_PATIENT_ALLERGIES_DETAIL_REQUEST } from './fetch-patient-allergies-detail.duck';
+import { fetchPatientAllergiesDetailRequest } from './fetch-patient-allergies-detail.duck';
 
 export const FETCH_PATIENT_ALLERGIES_REQUEST = 'FETCH_PATIENT_ALLERGIES_REQUEST';
 export const FETCH_PATIENT_ALLERGIES_SUCCESS = 'FETCH_PATIENT_ALLERGIES_SUCCESS';
@@ -32,24 +32,20 @@ export const fetchPatientAllergiesEpic = (action$, store) =>
 export const fetchPatientAllergiesUpdateEpic = (action$, store) =>
   action$.ofType(FETCH_PATIENT_ALLERGIES_UPDATE_REQUEST)
     .mergeMap(({ payload }) =>
-      ajax.getJSON(`${usersUrls.PATIENTS_URL}/${payload.userId}/allergies`, {
-        headers: { Cookie: store.getState().credentials.cookie },
-      })
-        .map((response) => {
+			ajax.getJSON(`${usersUrls.PATIENTS_URL}/${payload.userId}/allergies`, {
+				headers: { Cookie: store.getState().credentials.cookie },
+			})
+        .flatMap((response) => {
           const userId = payload.userId;
           const sourceId = payload.sourceId;
-          store.dispatch({
-            type: FETCH_PATIENT_ALLERGIES_DETAIL_REQUEST,
-            payload: { userId, sourceId },
-          });
-          return fetchPatientAllergiesSuccess({
-            userId,
-            allergies: response,
-          });
+
+          return [
+            fetchPatientAllergiesSuccess({ userId, allergies: response}),
+						fetchPatientAllergiesDetailRequest({ userId, sourceId })
+          ];
         })
         .catch(error => Observable.of(fetchPatientAllergiesFailure(error)))
     );
-
 export default function reducer(patientsAllergies = {}, action) {
   switch (action.type) {
     case FETCH_PATIENT_ALLERGIES_SUCCESS:
