b594e2e8012610f6c13c1362e92edf88c5cef0a9
Alex Honcharov <gonalex777@gmail.com>
2017-11-17 12:15:19 +0200

    Add Test Results plugin.

---
+++ src/components/pages/TestResults/forms.config.js
@@ -0,0 +1,37 @@
+export const valuesNames = {
+  NAME: 'testName',
+  STATUS: 'status',
+  TAKEN: 'sampleTaken',
+  CONCLUSION: 'conclusion',
+  AUTHOR: 'author',
+  DATE: 'currentDate',
+
+  TR: 'testResults',
+  TR_RESULT: 'result',
+  TR_VALUE: 'value',
+  TR_UNIT: 'unit',
+  TR_NORMAL: 'normalRange',
+  TR_COMMENT: 'comment',
+
+  SOURCE: 'source',
+  SOURCE_ID: 'sourceId',
+};
+
+export const valuesLabels = {
+  NAME: 'Test Name',
+  STATUS: 'Status',
+  TAKEN: 'Sample Taken',
+  CONCLUSION: 'Conclusion',
+  AUTHOR: 'Author',
+  DATE: 'Date',
+
+  TR: 'Test Results',
+  TR_RESULT: 'Results',
+  TR_VALUE: 'Value',
+  TR_UNIT: 'Unit',
+  TR_NORMAL: 'Normal Range',
+  TR_COMMENT: 'Comment',
+
+  SOURCE: 'Source',
+  SOURCE_ID: 'Source ID',
+};
