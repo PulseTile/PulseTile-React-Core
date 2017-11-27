11d9e647aeefada8c6431e697444932dfb15c70f
sliceart_polishchuk <dpolishchuk@sliceart.com.ua>
2017-11-27 13:28:26 +0200

    changes in the data connection to the <Main /> component

--- src/components/containers/App/App.js
+++ src/components/containers/App/App.js
@@ -1,37 +1,30 @@
 import React, { PureComponent } from 'react';
 import classNames from 'classnames';
 import LoadingBar from 'react-redux-loading-bar'
-import { connect } from 'react-redux';
-import _ from 'lodash/fp';
 
 import TopHeader from '../TopHeader/TopHeader';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../../presentational/Footer/Footer';
 import MainSpinner from '../MainSpinner/MainSpinner';
-import initialiseSelector from './selectors';
 
 import '../../../styles/main.scss';
 
-@connect(initialiseSelector)
-export default class App extends PureComponent {
-  render() {
-    const { initialiseData } = this.props;
-    const isTouchDevice = ('ontouchstart' in window) ? 'touch-device' : 'is-not-touch-device';
+const App = (props) => {
+  const isTouchDevice = ('ontouchstart' in window) ? 'touch-device' : 'is-not-touch-device';
 
-    return (
-      <div className="page">
-        <LoadingBar className="loading-bar" />
-        <MainSpinner />
-        <div className={classNames('wrapper', isTouchDevice)}>
-          <header className="header">
-            <TopHeader />
-            <Header />
-          </header>
-          {!_.isEmpty(initialiseData) ? <Main /> : null }
-        </div>
-        <Footer />
-      </div>
-    )
-  }
-}
+  return (<div className="page">
+    <LoadingBar className="loading-bar" />
+    <MainSpinner />
+    <div className={classNames('wrapper', isTouchDevice)}>
+      <header className="header">
+        <TopHeader />
+        <Header />
+      </header>
+      <Main />
+    </div>
+    <Footer />
+  </div>)
+};
+
+export default App;
--- src/components/containers/Main/Main.js
+++ src/components/containers/Main/Main.js
@@ -7,7 +7,7 @@ import _ from 'lodash/fp';
 
 import ProtectedRoute from './ProtectedRoute';
 import Breadcrumbs from '../Breadcumbs/Breadcrumbs';
-import { sidebarAndUserSelector, mainSelector } from './selectors';
+import { sidebarAndUserSelector, mainSelector, initialiseSelector } from './selectors';
 import { PatientsLists, SystemDashboard, PatientsFullDetailsSearch, UserProfile, PatientsSummary } from '../../pages';
 import { clientUrls } from '../../../config/client-urls.constants';
 import { routersPluginConfig } from '../../../plugins.config';
@@ -16,6 +16,7 @@ import { redirectAccordingRole } from '../../../utils/redirect-helpers.utils'
 @withRouter
 @connect(sidebarAndUserSelector)
 @connect(mainSelector)
+@connect(initialiseSelector)
 export default class Main extends PureComponent {
     static propTypes = {
       isSidebarVisible: PropTypes.bool.isRequired,
@@ -30,7 +31,7 @@ export default class Main extends PureComponent {
     }
 
     render() {
-      const { isSidebarVisible, userAccount, patientSummeriesParams } = this.props;
+      const { isSidebarVisible, userAccount, patientSummeriesParams, initialiseData } = this.props;
       const patientSummeries = _.head(_.values(patientSummeriesParams));
       return (
         <main className={classNames('main', { showSidebar: isSidebarVisible })}>
@@ -38,7 +39,7 @@ export default class Main extends PureComponent {
             userAccount={userAccount}
             patientSummeries={patientSummeries}
           />
-          <Switch>
+          {!_.isEmpty(initialiseData) ? <Switch>
             <Route exact path={clientUrls.USER_PROFILE} component={UserProfile} />
             <Route exact path={`${clientUrls.PATIENTS}/:userId/${clientUrls.PATIENTS_SUMMARY}`} component={PatientsSummary} />
             <ProtectedRoute exact path={clientUrls.PATIENTS} component={PatientsLists} userAccount={userAccount} />
@@ -46,7 +47,7 @@ export default class Main extends PureComponent {
             <ProtectedRoute exact path={clientUrls.CHARTS} component={SystemDashboard} userAccount={userAccount} />
             <ProtectedRoute exact path={clientUrls.ROOT} component={SystemDashboard} userAccount={userAccount} />
             {routersPluginConfig.map(item => <Route exact path={item.path} component={item.component} />)}
-          </Switch>
+          </Switch> : null }
         </main>
       )
     }
