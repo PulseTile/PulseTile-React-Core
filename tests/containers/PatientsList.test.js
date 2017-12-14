ec5a4ec4961a35a6fb01a5efc06d57bd6546bb6e
sliceart_polishchuk <dpolishchuk@sliceart.com.ua>
2017-12-14 18:41:56 +0200

    finished testing for containers in the project #78

--- tests/containers/PatientsList.test.js
+++ tests/containers/PatientsList.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, { mount, shallow, render } from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 
 import PatientsList from '../../src/components/containers/PatientsList/PatientsList';
@@ -19,11 +19,19 @@ const allPatientsWithCounts = [
     nhsNumber: '9999999006',
     pasNo: '595941',
     phone: '070 6691 5178',
-    dateOfBirthConvert: '24-Jun-1958',
-    diagnosesDateConvert: '12-Dec-2017',
-    ordersDateConvert: '12-Dec-2017',
-    resultsDateConvert: '12-Dec-2017',
-    vitalsDateConvert: '12-Dec-2017',
+  },
+  {
+    id: '9999999006',
+    address: 'P.O. Box 711, 8725 Purus Rd., Grangemouth, Stirlingshire, B4 8MW',
+    dateOfBirth: -363657600000,
+    department: 'Primary Care',
+    gender: 'Male',
+    gpAddress: 'Aulderan Practice, Ap #806-2884 Enim St., Auldearn, Nairnshire, V4F 9GJ',
+    gpName: 'Joseph May N.',
+    name: 'Ezra Gordon',
+    nhsNumber: '9999999006',
+    pasNo: '595941',
+    phone: '070 6691 5178',
   },
 ];
 const allPatients = [
@@ -39,11 +47,19 @@ const allPatients = [
     nhsNumber: '9999999006',
     pasNo: '595941',
     phone: '070 6691 5178',
-    dateOfBirthConvert: '24-Jun-1958',
-    diagnosesDateConvert: '12-Dec-2017',
-    ordersDateConvert: '12-Dec-2017',
-    resultsDateConvert: '12-Dec-2017',
-    vitalsDateConvert: '12-Dec-2017',
+  },
+  {
+    id: '9999999006',
+    address: 'P.O. Box 711, 8725 Purus Rd., Grangemouth, Stirlingshire, B4 8MW',
+    dateOfBirth: -363657600000,
+    department: 'Primary Care',
+    gender: 'Male',
+    gpAddress: 'Aulderan Practice, Ap #806-2884 Enim St., Auldearn, Nairnshire, V4F 9GJ',
+    gpName: 'Joseph May N.',
+    name: 'Ezra Gordon',
+    nhsNumber: '9999999006',
+    pasNo: '595941',
+    phone: '070 6691 5178',
   },
 ];
 const patientsCounts = {
@@ -66,10 +82,28 @@ describe('Component <PatientsList />', () => {
         allPatientsWithCounts={allPatientsWithCounts}
         handleHeaderCellClick={handleHeaderCellClick}
         allPatients={allPatients}
-        patientsPerPageAmount={10}
+        patientsPerPageAmount={1}
         panelTitle="test"
         patientsCounts={patientsCounts}
       />)
+    patientsList.find('.btn-dropdown-toggle').at(0).simulate('click');
+    patientsList.find('.btn-filter').at(0).simulate('click');
+
+    patientsList.find('.form-control').at(0).simulate('change', { target: { value: 'ezra' } });
+    expect(patientsList.state().nameShouldInclude).toEqual('ezra');
+    patientsList.find('th[name="address"]').simulate('click');
     expect(patientsList).toMatchSnapshot();
+
+    expect(patientsList.state().isDisclaimerModalVisible).toEqual(false);
+    patientsList.setState({ isDisclaimerModalVisible: true });
+    expect(patientsList.state().isDisclaimerModalVisible).toEqual(true);
+    patientsList.find('[name="address"]').at(0).simulate('click');
+    patientsList.find('.pagination-link').at(2).simulate('click');
+    patientsList.find('.btn-dropdown-toggle').at(3).simulate('click');
+    patientsList.setProps({ patientsPerPageAmount: 2 });
+    expect(patientsList.props().patientsPerPageAmount).toEqual(2);
+
+    patientsList.mount();
+    patientsList.unmount();
   });
 });
