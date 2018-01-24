b77eddc31d5840bb002d24a377cd925011283ed4
sliceart_polishchuk <dpolishchuk@sliceart.com.ua>
2018-01-24 14:12:37 +0200

    update styles for swiper and add dragable for <Images /> #92

--- src/components/pages/Images/ImagesDetail/ImagesDetail.js
+++ src/components/pages/Images/ImagesDetail/ImagesDetail.js
@@ -6,32 +6,100 @@ import moment from 'moment'
 import PluginDetailPanel from '../../../plugin-page-component/PluginDetailPanel'
 import ImagesDetailPanel from '../images-page-component/ImagesDetailPanel'
 import { valuesNames, valuesLabels } from '../forms.config';
+import { hasClass } from '../../../../utils/plugin-helpers.utils';
 
 const IMAGES_PANEL = 'imagesPanel';
 const IMAGES_DETAIL_PANEL = 'imagesDetailPanel';
-const SCALE = 50;
+const SCALE = 100;
 
 export default class ImagesDetail extends PureComponent {
   getURLtoImage = id => `http://46.101.95.245/orthanc/instances/${id}/preview`;
 
   state = {
     styleSwiper: {},
+    coordX: '',
+    coordY: '',
+    drag: false,
+    targ: {},
+    marginTop: 0,
+    marginLeft: 0,
+    touchMode: true,
   };
 
   componentWillReceiveProps(nextProps) {
-    if(nextProps.instanceIds[0] !== this.props.instanceIds[0]) {
-      this.setState({styleSwiper : {}})
+    if (nextProps.instanceIds[0] !== this.props.instanceIds[0]) {
+      this.setState({ styleSwiper: {}, touchMode: true });
+      this.swiper.allowTouchMove = true;
     }
   }
+
+  componentDidMount() {
+    document.onmousedown = this.startDrag;
+    document.onmouseup = this.stopDrag;
+  }
+
+  startDrag = (e) => {
+    if (!this.state.touchMode) {
+      if (e.preventDefault) e.preventDefault();
+
+      let target = e.target ? e.target : e.srcElement;
+
+      if (hasClass(target, 'swiper-slide')) {
+        target = target.querySelector('img');
+      }
+
+      if (target.id !== `img-${this.swiper.activeIndex}`) { return }
+
+      this.setState({ targ: target });
+
+      if (target.id !== `img-${this.swiper.activeIndex}`) { return }
+      const marginTop = parseInt(target.style.marginTop) || 0;
+      const marginLeft = parseInt(target.style.marginLeft) || 0;
+      this.setState({
+        coordX: parseInt(e.clientX),
+        coordY: parseInt(e.clientY),
+        drag: true,
+        marginTop,
+        marginLeft,
+      });
+
+      document.onmousemove = this.dragDiv;
+
+      return false;
+    }
+  };
+
+  dragDiv = (e) => {
+    const { drag, coordX, coordY, styleSwiper, marginTop, marginLeft } = this.state;
+    if (!drag) { return }
+    if (!e) { const e = window.event }
+    const currentCoordX = e.clientX;
+    const currentCoordY = e.clientY;
+    this.setState({ styleSwiper: {
+      ...styleSwiper,
+      [this.swiper.activeIndex]: {
+        ...styleSwiper[this.swiper.activeIndex],
+        marginLeft: `${marginLeft - coordX + currentCoordX}px`,
+        marginTop: `${marginTop - coordY + currentCoordY}px`,
+      },
+    } });
+    return false;
+  };
+
+  stopDrag = () => {
+    this.setState({ drag: false });
+  };
 
   zoomin = () => {
     const { styleSwiper } = this.state;
     const myImg = document.getElementById(`img-${this.swiper.activeIndex}`);
     const currWidth = myImg.clientWidth;
     const currHeight = myImg.clientHeight;
+
     this.setState({ styleSwiper: {
       ...styleSwiper,
       [this.swiper.activeIndex]: {
+        ...styleSwiper[this.swiper.activeIndex],
         width: `${currWidth + SCALE}px`,
         height: `${currHeight + SCALE}px`,
         maxWidth: 'initial',
@@ -47,6 +115,7 @@ export default class ImagesDetail extends PureComponent {
     this.setState({ styleSwiper: {
       ...styleSwiper,
       [this.swiper.activeIndex]: {
+        ...styleSwiper[this.swiper.activeIndex],
         width: `${currWidth - SCALE}px`,
         height: `${currHeight - SCALE}px`,
         maxWidth: 'initial',
@@ -55,9 +124,19 @@ export default class ImagesDetail extends PureComponent {
     } });
   };
 
+  moveImg = () => {
+    this.setState({ touchMode: false });
+    this.swiper.allowTouchMove = false;
+  };
+
+  fadeImg = () => {
+    this.setState({ touchMode: true });
+    this.swiper.allowTouchMove = true;
+  };
+
   render() {
     const { onExpand, onShow, openedPanel, expandedPanel, currentPanel, onEdit, editedPanel, instanceIds } = this.props;
-    const { styleSwiper } = this.state;
+    const { styleSwiper, touchMode } = this.state;
     let { detail } = this.props;
     detail = detail || {};
     const seriesDate = (detail[valuesNames.SERIES_DATE]) ? moment(detail[valuesNames.SERIES_DATE]).format('DD-MMM-YYYY') : '';
@@ -75,9 +154,13 @@ export default class ImagesDetail extends PureComponent {
       },
       spaceBetween: 30,
       init: true,
+      allowTouchMove: touchMode,
     };
 
-    const swiperSlide = instanceIds.map((item, index) => {
+    const demoArraySlides = ['d1d4875a-2a8267c2-2ee4f8f0-3d9e917e-9a141930', 'f46068b5-95796c31-d6e3ddea-52d2fe09-72434be2'];
+    const demoInstanceIds = instanceIds.concat(demoArraySlides);
+
+    const swiperSlide = demoInstanceIds.map((item, index) => {
       return (<div key={index}><img id={`img-${index}`} style={styleSwiper[index] ? styleSwiper[index] : null} src={this.getURLtoImage(item)} /></div>)
     });
 
@@ -94,6 +177,8 @@ export default class ImagesDetail extends PureComponent {
             onShow={onShow}
             zoomin={this.zoomin}
             zoomout={this.zoomout}
+            moveImg={this.moveImg}
+            fadeImg={this.fadeImg}
           >
             <div className="panel-body-inner">
               <div className="form">
