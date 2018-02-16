10ee4a0bc02157491715e3a525beed0959d3cd3c
sliceart_polishchuk <dpolishchuk@sliceart.com.ua>
2018-02-16 12:04:24 +0200

    create ducks for <Proms /> #119

---
+++ src/components/pages/PROMs/ducks/fetch-patient-proms-detail-edit.duck.js
@@ -0,0 +1,43 @@
+import { Observable } from 'rxjs';
+import { ajax } from 'rxjs/observable/dom/ajax';
+import { createAction } from 'redux-actions';
+
+import { usersUrls } from '../../../../config/server-urls.constants'
+import { fetchPatientPromsUpdateRequest } from './fetch-patient-proms.duck'
+import { handleErrors } from '../../../../ducks/handle-errors.duck';
+
+export const FETCH_PATIENT_PROMS_DETAIL_EDIT_REQUEST = 'FETCH_PATIENT_PROMS_DETAIL_EDIT_REQUEST';
+export const FETCH_PATIENT_PROMS_DETAIL_EDIT_SUCCESS = 'FETCH_PATIENT_PROMS_DETAIL_EDIT_SUCCESS';
+export const FETCH_PATIENT_PROMS_DETAIL_EDIT_FAILURE = 'FETCH_PATIENT_PROMS_DETAIL_EDIT_FAILURE';
+
+export const fetchPatientPromsDetailEditRequest = createAction(FETCH_PATIENT_PROMS_DETAIL_EDIT_REQUEST);
+export const fetchPatientPromsDetailEditSuccess = createAction(FETCH_PATIENT_PROMS_DETAIL_EDIT_SUCCESS);
+export const fetchPatientPromsDetailEditFailure = createAction(FETCH_PATIENT_PROMS_DETAIL_EDIT_FAILURE);
+
+export const fetchPatientPromsDetailEditEpic = (action$, store) =>
+  action$.ofType(FETCH_PATIENT_PROMS_DETAIL_EDIT_REQUEST)
+    .mergeMap(({ payload }) =>
+      ajax.put(`${usersUrls.PATIENTS_URL}/${payload.userId}/proms/${payload.sourceId}`, payload, {
+        Cookie: store.getState().credentials.cookie,
+        'Content-Type': 'application/json',
+      })
+        .flatMap(({ response }) => {
+          const userId = payload.userId;
+          const sourceId = payload.sourceId;
+
+          return [
+            fetchPatientPromsDetailEditSuccess(response),
+            fetchPatientPromsUpdateRequest({ userId, sourceId }),
+          ];
+        })
+        // .catch(error => Observable.of(handleErrors(error)))
+    );
+
+export default function reducer(promsDetailEdit = {}, action) {
+  switch (action.type) {
+    case FETCH_PATIENT_PROMS_DETAIL_EDIT_SUCCESS:
+      return action.payload;
+    default:
+      return promsDetailEdit;
+  }
+}
---
+++ src/components/pages/PROMs/ducks/fetch-patient-proms-detail.duck.js
@@ -0,0 +1,37 @@
+import _ from 'lodash/fp';
+import { Observable } from 'rxjs';
+import { ajax } from 'rxjs/observable/dom/ajax';
+import { createAction } from 'redux-actions';
+
+import { usersUrls } from '../../../../config/server-urls.constants'
+import { handleErrors } from '../../../../ducks/handle-errors.duck';
+
+export const FETCH_PATIENT_PROMS_DETAIL_REQUEST = 'FETCH_PATIENT_PROMS_DETAIL_REQUEST';
+export const FETCH_PATIENT_PROMS_DETAIL_SUCCESS = 'FETCH_PATIENT_PROMS_DETAIL_SUCCESS';
+export const FETCH_PATIENT_PROMS_DETAIL_FAILURE = 'FETCH_PATIENT_PROMS_DETAIL_FAILURE';
+
+export const fetchPatientPromsDetailRequest = createAction(FETCH_PATIENT_PROMS_DETAIL_REQUEST);
+export const fetchPatientPromsDetailSuccess = createAction(FETCH_PATIENT_PROMS_DETAIL_SUCCESS);
+export const fetchPatientPromsDetailFailure = createAction(FETCH_PATIENT_PROMS_DETAIL_FAILURE);
+
+export const fetchPatientPromsDetailEpic = (action$, store) =>
+  action$.ofType(FETCH_PATIENT_PROMS_DETAIL_REQUEST)
+    .mergeMap(({ payload }) =>
+      ajax.getJSON(`${usersUrls.PATIENTS_URL}/${payload.userId}/proms/${payload.sourceId}`, {
+        headers: { Cookie: store.getState().credentials.cookie },
+      })
+        .map(response => fetchPatientPromsDetailSuccess({
+          userId: payload.userId,
+          promsDetail: response,
+        }))
+        // // .catch(error => Observable.of(handleErrors(error)))
+    );
+
+export default function reducer(promsDetail = {}, action) {
+  switch (action.type) {
+    case FETCH_PATIENT_PROMS_DETAIL_SUCCESS:
+      return _.set(action.payload.userId, action.payload.promsDetail, promsDetail);
+    default:
+      return promsDetail;
+  }
+}
