890861291c9bd4ed7a074ba0fb2c7df585554410
Alexey Honcharov <gonalex777@gmail.com>
2018-02-24 14:36:48 +0200

    Add jQuery variable to global environment and update tests.

--- jest-config.js
+++ jest-config.js
@@ -10,6 +10,7 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/fileTransformer.js',
     '^.+\\.js$': 'babel-jest',
   },
+  setupFiles: ['./test-configs/setup-jest.js'],
   collectCoverageFrom: [
     // Coverage files
     '**/src/components/presentational/**.{js,jsx}',
---
+++ test-configs/setup-jest.js
@@ -0,0 +1,3 @@
+import jquery from 'jquery';
+global.$ = global.jQuery = jquery;
+window.$ = window.jQuery = jquery;
