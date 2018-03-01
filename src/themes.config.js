f0b95175be4cb8b82745e78d09938a0fb53d8bfe
Alexey Honcharov <gonalex777@gmail.com>
2018-03-01 20:36:38 +0200

    Hide vaccinations board on the Patients Summary page for the Main theme.

--- src/themes.config.js
+++ src/themes.config.js
@@ -14,6 +14,11 @@ const mainThemeConfigs = {
 
   patientsSummaryPageName: 'Patient Summary',
   patientsSummaryHasPreviewSettings: false,
+
+  dashboardBeing: {
+    vaccinations: false,
+    topThreeThings: false,
+  },
 };
 
 // - In config - ./plugins.config.js when you will change sidebarConfig
