c7a9067614463ba65829b2f136bc67f232a33443
Alexey Honcharov <gonalex777@gmail.com>
2018-03-01 21:45:21 +0200

    Change default Values for forms of Allergies Medications and Problems Headings.

--- src/components/pages/Medications/MedicationsCreate/default-values.config.js
+++ src/components/pages/Medications/MedicationsCreate/default-values.config.js
@@ -4,6 +4,8 @@ export const defaultFormValues = {
   [valuesNames.AUTHOR]: 'bob.smith@gmail.com',
   [valuesNames.SOURCE_ID]: '',
   [valuesNames.ISIMPORT]: false,
+  [valuesNames.ORIGINAL_SOURCE]: '',
+  [valuesNames.ORIGINAL_COMPOSITION]: '',
   [valuesNames.MEDICATION_CODE]: 173134014,
   [valuesNames.MEDICATION_TERMINOLOGY]: 'SNOMED-CT',
 };
