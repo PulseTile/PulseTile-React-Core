6e394195953c8f2bf14de6a38f01876face87178
bogdan_scher <bsc@piogroup.com>
2018-09-10 15:57:13 +0300

    for tour

--- src/components/containers/TopHeader/TourSteps.js
+++ src/components/containers/TopHeader/TourSteps.js
@@ -5,16 +5,9 @@ function getStepsArray(steps) {
     for (let i = 0, n = steps.length; i < n; i++) {
         let item = steps[i];
         result[i] = {
-            title: (
-                <h2 align="left">{item.title}</h2>
-            ),
+            title: item.title,
             target: item.target,
-            content: (
-                <div>
-                    <h4 align="left">{item.lowTitle}</h4>
-                    <p>{item.content}</p>
-                </div>
-            ),
+            content: item.content,
             placement: item.placement,
             disableOverlayClicks: true,
             showSkipButton: true,
