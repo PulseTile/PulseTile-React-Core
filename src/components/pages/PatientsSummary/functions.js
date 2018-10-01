b37191eb637108f9f2f9e8c2d55ef00f049696ec
bogdan_scher <bsc@piogroup.com>
2018-10-01 13:07:48 +0300

    chenges for Feeds

--- src/components/pages/PatientsSummary/functions.js
+++ src/components/pages/PatientsSummary/functions.js
@@ -7,19 +7,37 @@ import { themeConfigs } from '../../../themes.config';
  *
  * @return {number}
  */
-export function getInitialPanelsNumber() {
-    const defaultPanelsNumber = 4;
-    const hiddenCorePlugins = get(themeConfigs, 'corePluginsToHide', []);
-    return defaultPanelsNumber - hiddenCorePlugins.length;
+function getInitialPanelsNumber() {
+  const defaultPanelsNumber = 4;
+  const hiddenCorePlugins = get(themeConfigs, 'corePluginsToHide', []);
+  return defaultPanelsNumber - hiddenCorePlugins.length;
+}
+
+
+/**
+ * This function return the number of synopsis panels for none-core plugins
+ *
+ * @return {number}
+ */
+function getNonCorePanelsNumber(testStoreContent) {
+  let result = 0;
+  const plugins = Object.keys(testStoreContent);
+  plugins.forEach(item => {
+    if (item !== 'fetchFeedsRequest') {
+      result++;
+    }
+  });
+  return result;
 }
 
 /**
+ * This function return the total number of synopsis panels
  *
  * @param testStoreContent
- * @return {*}
+ * @return {number}
  */
 export function getPanelsNumber(testStoreContent) {
-    const initialNumber = getInitialPanelsNumber();
-    const pluginsNumber = Object.keys(testStoreContent).length;
-    return (pluginsNumber > 0) ? (initialNumber + pluginsNumber) : initialNumber;
+  const initialNumber = getInitialPanelsNumber();
+  const pluginsNumber = getNonCorePanelsNumber(testStoreContent);
+  return (pluginsNumber > 0) ? (initialNumber + pluginsNumber) : initialNumber;
 }
