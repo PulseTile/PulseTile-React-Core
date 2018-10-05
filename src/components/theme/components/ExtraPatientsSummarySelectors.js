635021560d60cb6a10b80a1dee9e386049ad5e14
bogdan_scher <bsc@piogroup.com>
2018-10-05 14:58:24 +0300

    remove all parts of Feeds-plugin

---
+++ src/components/theme/components/ExtraPatientsSummarySelectors.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/**
+ * This component returns list of selectors, which creates separately (like Feeds-selectors)
+ *
+ * @param props
+ * @return {XML}
+ */
+const ExtraPatientsSummarySelectors = props => {
+    return null;
+};
+
+export default ExtraPatientsSummarySelectors;
