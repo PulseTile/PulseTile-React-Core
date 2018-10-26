adf9f296bafd1866cebec6be598beabb571baf92
olegmdev <qu1ze@mail.ru>
2018-10-26 18:08:59 +0300

    added IE plugin

--- src/components/containers/App/App.js
+++ src/components/containers/App/App.js
@@ -1,61 +1,76 @@
-import React, { Component } from 'react';
-import classNames from 'classnames';
-import LoadingBar from 'react-redux-loading-bar';
-import { connect } from 'react-redux';
-import _ from 'lodash/fp';
-import { withRouter } from 'react-router-dom';
-import { compose } from 'recompose';
-import { themeConfigs } from '../../../themes.config';
-import { requestErrorSelector, patientInfoSelector } from './selectors';
-import TopHeader from '../TopHeader/TopHeader';
-import Header from '../Header/Header';
-import Main from '../Main/Main';
-import Footer from '../../presentational/Footer/Footer';
-import MainSpinner from '../MainSpinner/MainSpinner';
-import HandleErrors from '../HandleErrors/HandleErrors';
-import HeaderList from '../HeaderList/HeaderList';
-import ExtraPlugins from '../../theme/components/ExtraPlugins';
-import { image } from './HeaderImage';
-import IE from '../../pages/IE/index';
+import React, { Component } from "react";
+import classNames from "classnames";
+import LoadingBar from "react-redux-loading-bar";
+import { connect } from "react-redux";
+import _ from "lodash/fp";
+import { withRouter } from "react-router-dom";
+import { compose } from "recompose";
+import { themeConfigs } from "../../../themes.config";
+import { requestErrorSelector, patientInfoSelector } from "./selectors";
+import TopHeader from "../TopHeader/TopHeader";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../../presentational/Footer/Footer";
+import MainSpinner from "../MainSpinner/MainSpinner";
+import HandleErrors from "../HandleErrors/HandleErrors";
+import HeaderList from "../HeaderList/HeaderList";
+import ExtraPlugins from "../../theme/components/ExtraPlugins";
+import { image } from "./HeaderImage";
+import IE from "../../pages/IE/index";
 
-import '../../../config/styles';
+import "../../../config/styles";
 
 export class App extends Component {
   render() {
     const { requestError, patientsInfo } = this.props;
-    const isTouchDevice = (this.props.isTouchDevice) ? 'touch-device' : ('ontouchstart' in window) ? 'touch-device' : 'is-not-touch-device';
-    const UA = window.navigator.userAgent.toLowerCase()
-    const isIE = (/trident/gi).test(UA) || (/msie/gi).test(UA);
+    const isTouchDevice = this.props.isTouchDevice
+      ? "touch-device"
+      : "ontouchstart" in window
+        ? "touch-device"
+        : "is-not-touch-device";
+    const UA = window.navigator.userAgent.toLowerCase();
+    const isIE = /trident/gi.test(UA) || /msie/gi.test(UA);
     return (
-      <div className="page">
-        <LoadingBar className="loading-bar" />
-        {!_.isEmpty(requestError) ? <HandleErrors /> : <MainSpinner /> }
-        {!_.isEmpty(patientsInfo) ? <div style={{height: '100%'}}>
-          <div className={classNames('wrapper', isTouchDevice)}>
-            <header className="header">
-              <TopHeader
-                isHasSearch={themeConfigs.headerHasSearch}
-              >
-                {themeConfigs.isLeedsPHRHeaderList ?
-                  <HeaderList items={[
-                    <img src={image} alt="header img 2" />,
-                  ]}
-                  />
-                  : <div />}
-              </TopHeader>
-              <Header />
-            </header>
-            <Main />
-            <ExtraPlugins />
+      <div>
+        {isIE && <IE />}
+        {!isIE && (
+          <div className="page">
+            <LoadingBar className="loading-bar" />
+            {!_.isEmpty(requestError) ? <HandleErrors /> : <MainSpinner />}
+            {!_.isEmpty(patientsInfo) ? (
+              <div style={{ height: "100%" }}>
+                <div className={classNames("wrapper", isTouchDevice)}>
+                  <header className="header">
+                    <TopHeader isHasSearch={themeConfigs.headerHasSearch}>
+                      {themeConfigs.isLeedsPHRHeaderList ? (
+                        <HeaderList
+                          items={[<img src={image} alt="header img 2" />]}
+                        />
+                      ) : (
+                        <div />
+                      )}
+                    </TopHeader>
+                    <Header />
+                  </header>
+                  <Main />
+                  <ExtraPlugins />
+                </div>
+                <Footer
+                  copyright={themeConfigs.footerCopyright}
+                  isShowSupportedBy={themeConfigs.footerHasShowSupportedByText}
+                />
+              </div>
+            ) : null}
           </div>
-          <Footer
-            copyright={themeConfigs.footerCopyright}
-            isShowSupportedBy={themeConfigs.footerHasShowSupportedByText}
-          />
-        </div> : null}
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default withRouter(compose(connect(requestErrorSelector), connect(patientInfoSelector))(App))
+export default withRouter(
+  compose(
+    connect(requestErrorSelector),
+    connect(patientInfoSelector)
+  )(App)
+);
